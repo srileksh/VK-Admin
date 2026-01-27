41f24662529e95913f9de6fe6b0bc40206c9958b
Fathima F <fathimafasal0698@gmail.com>
2026-01-27 19:38:54 +0530

    edited create course component

--- src/components/Course/CourseWizard.jsx
+++ src/components/Course/CourseWizard.jsx
@@ -1,43 +1,4 @@
-// "use client";
-// import { useState } from "react";
-// import CreateCourse from "./CreateCourse";
-// import CourseContent from "./CourseContent";
-// import CreateModules from "./CreateModules";
 
-// export default function CourseWizard({ onClose }) {
-//   const [step, setStep] = useState(1);
-//   const [courseId, setCourseId] = useState(null);
-
-//   return (
-//     <>
-//       {step === 1 && (
-//         <CreateCourse
-//   onCancel={onClose}
-//   onSuccess={(courseId) => {
-//     setCourseId(courseId);
-//     setStep(2);
-//   }}
-// />
-//       )}
-
-//       {step === 2 && (
-//         <CourseContent
-//           courseId={courseId}
-//           onBack={() => setStep(1)}
-//           onNext={() => setStep(3)}
-//         />
-//       )}
-
-//       {step === 3 && (
-//         <CreateModules
-//           courseId={courseId}
-//           onCancel={() => setStep(2)}
-//           onFinish={onClose}
-//         />
-//       )}
-//     </>
-//   );
-// }
 "use client";
 import { useState } from "react";
 import CreateCourse from "./CreateCourse";
