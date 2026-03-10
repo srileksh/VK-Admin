fec4570e4d68472fa141098cde5cdecfd9ddf096
Fathima F <fathimafasal0698@gmail.com>
2026-03-10 15:02:06 +0530

    Comments cleared

--- src/services/EnquiryApi.js
+++ src/services/EnquiryApi.js
@@ -1,27 +1,4 @@
-// import axiosInstance from "./axios";
 
-// // Get all enquiries
-// export const getEnquiries = async () => {
-//   const res = await axiosInstance.get("/admin/contact-messages");
-//   return res.data.data;
-// };
-
-// // Get single enquiry
-// export const getEnquiryById = async (id) => {
-//   const res = await axiosInstance.get(`/admin/contact-messages/${id}`);
-//   return res.data.data;
-// };
-
-// // Update enquiry status
-// export const updateEnquiryStatus = async (id, status) => {
-//   const res = await axiosInstance.patch(
-//     `/admin/contact-messages/${id}/status`,
-//     { status }
-//   );
-  
-
-//   return res.data.data;
-// };
 
 import axiosInstance from "./axios";
 
